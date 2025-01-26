8df587e9772e5c5c127a0d4c96696563d5e9f27c
KrNitesh <knitesh840.nk@gmail.com>
2025-01-27 05:04:12 +0530

    second line removed

--- Demo.java
+++ Demo.java
@@ -2,7 +2,6 @@ public class FirstProgram {
       public static void main(String [] args){
          public void add(){
           //our logic will be here
-          // our second logic will be here
          }
          System.out.println("Hi Everyone!!");
      }
