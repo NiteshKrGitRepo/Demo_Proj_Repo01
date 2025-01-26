f1da99710165832531bc1dc7be9d4e01879359f9
KrNitesh <knitesh840.nk@gmail.com>
2025-01-27 03:45:37 +0530

    add method is added

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class FirstProgram {
       public static void main(String [] args){
+         public void add(){
+          //our logic will be here
+         }
          System.out.println("Hi Everyone!!");
      }
 }
