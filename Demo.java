7ca21f5f4ed2ac1f29496e6632c7e5a10a1ea6e2
KrNitesh <knitesh840.nk@gmail.com>
2025-01-27 05:00:09 +0530

    1 line updated

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ public class FirstProgram {
       public static void main(String [] args){
          public void add(){
           //our logic will be here
+          // our second logic will be here
          }
          System.out.println("Hi Everyone!!");
      }
